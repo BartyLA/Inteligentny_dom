b4b6bd2486a322f8a49a4f8daa6fc9ce191ace1b
Bartek <macbookpro15@MacBook-Pro-Macbook-3.local>
2016-01-23 14:58:59 +0100

    Generating Scoffolds for Room, Category, Room_Category and Device

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :devices
+  resources :room_categories
+  resources :categories
+  resources :rooms
   root 'main#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
